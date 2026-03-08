5d351bd47b7bedb9a1d50e7ea3d8f70bb3eaf059
nghia203 <99585411+nghia203@users.noreply.github.com>
2026-03-08 21:56:08 +0700

    Add solution for elu-activation

---
+++ elu-activation/elu-activation.py
@@ -0,0 +1,8 @@
+import numpy as np
+def elu(x, alpha):
+    """
+    Apply ELU activation to each element.
+    """
+    # Write code here
+    x = np.asarray(x)
+    return (np.where(x > 0, x, alpha * (np.exp(x) - 1))).tolist()
