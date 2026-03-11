526cdfe94a125a41f551a8f5d87b7fec7d452cdb
nghia203 <99585411+nghia203@users.noreply.github.com>
2026-03-11 09:48:07 +0700

    Add solution for wasserstein-critic-loss

---
+++ wasserstein-critic-loss/wasserstein-critic-loss.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def wasserstein_critic_loss(real_scores, fake_scores):
+    """
+    Compute Wasserstein Critic Loss for WGAN.
+    """
+    # Write code here
+    real_scores = np.asarray(real_scores)
+    fake_scores = np.asarray(fake_scores)
+    real_scores_mean = np.mean(real_scores)
+    fake_scores_mean = np.mean(fake_scores)
+    return fake_scores_mean - real_scores_mean
+    pass
