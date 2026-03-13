1fdc1c02ae36d404c233642d477fad22edb9ad78
nghia203 <99585411+nghia203@users.noreply.github.com>
2026-03-13 10:43:54 +0700

    Update solution for simple-moving-average

--- simple-moving-average/simple-moving-average.py
+++ simple-moving-average/simple-moving-average.py
@@ -1,12 +1,13 @@
 import numpy as np
+
 def simple_moving_average(values, window_size):
     """
     Compute the simple moving average of the given values.
     """
-    # Write code here
-    sma = []
     values = np.asarray(values)
-    for i in range(len(values) - window_size + 1):
-        window = values[i : i + window_size]
-        sma.append(np.sum(window) / window_size)
-    return sma
+    
+    weights = np.ones(window_size) / window_size
+    
+    sma = np.convolve(values, weights, mode='valid')
+    
+    return sma.tolist()
