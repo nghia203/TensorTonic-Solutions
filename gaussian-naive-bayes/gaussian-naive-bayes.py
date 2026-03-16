a4e29f6e8c75111ce0f7cb87355a6d99fbd7b50a
nghia203 <99585411+nghia203@users.noreply.github.com>
2026-03-16 17:22:01 +0700

    Update solution for gaussian-naive-bayes

--- gaussian-naive-bayes/gaussian-naive-bayes.py
+++ gaussian-naive-bayes/gaussian-naive-bayes.py
@@ -5,14 +5,11 @@ def gaussian_naive_bayes(X_train, y_train, X_test):
     Predict class labels for test samples using Gaussian Naive Bayes.
     """
     # Write code here
-    X_train = np.asarray(X_train)
-    y_train = np.asarray(y_train)
-    X_test = np.asarray(X_test)
-    
+    X_train, y_train, X_test = map(np.asarray, [X_train, y_train, X_test])
     classes = np.unique(y_train)
-    n_features = X_train.shape[1]
-
+    n_test = X_test.shape[0]
     stats = {}
+    
     for c in classes:
         X_c = X_train[y_train == c]
         stats[c] = {
@@ -20,21 +17,17 @@ def gaussian_naive_bayes(X_train, y_train, X_test):
             "var": np.var(X_c, axis = 0) + 1e-9,
             "prior": X_c.shape[0] / X_train.shape[0]
         }
-    def cal_log_likelihood(x, mean, var):
-        exp = -((x - mean) ** 2) / (var * 2)
-        log_cal = -np.log(2 * np.pi * var) / 2 + exp
-        return np.sum(log_cal)
-
-    prediction = []
-    for x in X_test:
-        posteriors = []
-        for c in classes:
-            prior = np.log(stats[c]["prior"])
-            likelihood = cal_log_likelihood(x, stats[c]["mean"], stats[c]["var"])
-            posteriors.append(prior + likelihood)
-
-        prediction.append(classes[np.argmax(posteriors)])
-
-    return prediction
         
-    
+    log_posteriors = np.zeros((n_test, len(classes)))
+
+    for i, c in enumerate(classes):
+        mean = stats[c]["mean"]
+        var = stats[c]["var"]
+        prior = stats[c]["prior"]
+        
+        exponent = -0.5 * np.sum(((X_test - mean) ** 2) / var, axis=1)
+        log_prob = -0.5 * np.sum(np.log(2 * np.pi * var))
+        
+        log_posteriors[:, i] = prior + log_prob + exponent
+
+    return classes[np.argmax(log_posteriors, axis=1)].tolist()
