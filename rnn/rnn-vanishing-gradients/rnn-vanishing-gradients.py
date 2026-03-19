fef06747f31744fd4715c9ebc12268dd38371dc3
nghia203 <99585411+nghia203@users.noreply.github.com>
2026-03-19 12:37:09 +0700

    Add solution for rnn-vanishing-gradients

---
+++ rnn/rnn-vanishing-gradients/rnn-vanishing-gradients.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def compute_gradient_norm_decay(T: int, W_hh: np.ndarray) -> list:
+    """
+    Simulate gradient norm decay over T time steps.
+    Returns list of gradient norms.
+    """
+    # YOUR CODE HERE
+    grad_prev = 1.0
+    grad_list = [(grad_prev)]
+    for t in range(1, T):
+        norm = np.linalg.norm(W_hh, ord = 2)
+        grad = grad_prev * norm
+        grad_list.append(grad)
+        grad_prev = grad
+    return grad_list
+    pass
